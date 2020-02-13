95a9bfccac7812f1e6dd86c72e5111c1b234a73f
Hanna <hannamatildawhite@gmail.com>
2020-02-13 19:34:37 +0100

    formated files with prettier.

--- fun.js
+++ fun.js
@@ -1,91 +1,91 @@
 if (window.innerWidth > 1300) {
-  /* This script supports IE9+ */
-  ;(function() {
-    const imageHolder = document.querySelector('.modal__imageHolder img')
-    const imageData = {
-      1: 'bilder/napoleon.gif',
-      2: 'bilder/soda_good.gif',
-      3: 'bilder/branding.gif',
-      4: 'bilder/musicplayer.gif',
-      5: 'bilder/gothenburg_fish.gif',
-      6: 'bilder/instruktion.gif'
-    }
-    // console.log(imageHolder)
+	/* This script supports IE9+ */
+	;(function() {
+		const imageHolder = document.querySelector('.modal__imageHolder img')
+		const imageData = {
+			1: 'bilder/napoleon.gif',
+			2: 'bilder/soda_good.gif',
+			3: 'bilder/branding.gif',
+			4: 'bilder/musicplayer.gif',
+			5: 'bilder/gothenburg_fish.gif',
+			6: 'bilder/instruktion.gif'
+		}
+		// console.log(imageHolder)
 
-    /* Opening modal window function */
-    function openModal() {
-      /* Get trigger element */
-      const modalTrigger = document.getElementsByClassName('jsModalTrigger')
+		/* Opening modal window function */
+		function openModal() {
+			/* Get trigger element */
+			const modalTrigger = document.getElementsByClassName('jsModalTrigger')
 
-      /* Set onclick event handler for all trigger elements */
-      for (let i = 0; i < modalTrigger.length; i++) {
-        modalTrigger[i].onclick = function() {
-          const target = this.getAttribute('href').substr(1)
-          const imageTarget = this.dataset.fun
-          const modalWindow = document.getElementById(target)
-          const imageToAdd = Object.keys(imageData).filter(
-            image => image === imageTarget
-          )
-          imageHolder.src = imageData[imageToAdd[0]]
+			/* Set onclick event handler for all trigger elements */
+			for (let i = 0; i < modalTrigger.length; i++) {
+				modalTrigger[i].onclick = function() {
+					const target = this.getAttribute('href').substr(1)
+					const imageTarget = this.dataset.fun
+					const modalWindow = document.getElementById(target)
+					const imageToAdd = Object.keys(imageData).filter(
+						image => image === imageTarget
+					)
+					imageHolder.src = imageData[imageToAdd[0]]
 
-          modalWindow.classList
-            ? modalWindow.classList.add('open')
-            : (modalWindow.className += ' ' + 'open')
-        }
-      }
-    }
+					modalWindow.classList
+						? modalWindow.classList.add('open')
+						: (modalWindow.className += ' ' + 'open')
+				}
+			}
+		}
 
-    function closeModal() {
-      /* Get close button */
-      const closeButton = document.getElementsByClassName('jsModalClose')
-      const closeOverlay = document.getElementsByClassName('jsOverlay')
+		function closeModal() {
+			/* Get close button */
+			const closeButton = document.getElementsByClassName('jsModalClose')
+			const closeOverlay = document.getElementsByClassName('jsOverlay')
 
-      /* Set onclick event handler for close buttons */
-      for (let i = 0; i < closeButton.length; i++) {
-        closeButton[i].onclick = function() {
-          const modalWindow = this.parentNode.parentNode
+			/* Set onclick event handler for close buttons */
+			for (let i = 0; i < closeButton.length; i++) {
+				closeButton[i].onclick = function() {
+					const modalWindow = this.parentNode.parentNode
 
-          modalWindow.classList
-            ? modalWindow.classList.remove('open')
-            : (modalWindow.className = modalWindow.className.replace(
-                new RegExp(
-                  '(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)',
-                  'gi'
-                ),
-                ' '
-              ))
-        }
-      }
+					modalWindow.classList
+						? modalWindow.classList.remove('open')
+						: (modalWindow.className = modalWindow.className.replace(
+								new RegExp(
+									'(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)',
+									'gi'
+								),
+								' '
+						  ))
+				}
+			}
 
-      /* Set onclick event handler for modal overlay */
-      for (let i = 0; i < closeOverlay.length; i++) {
-        closeOverlay[i].onclick = function() {
-          const modalWindow = this.parentNode
+			/* Set onclick event handler for modal overlay */
+			for (let i = 0; i < closeOverlay.length; i++) {
+				closeOverlay[i].onclick = function() {
+					const modalWindow = this.parentNode
 
-          modalWindow.classList
-            ? modalWindow.classList.remove('open')
-            : (modalWindow.className = modalWindow.className.replace(
-                new RegExp(
-                  '(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)',
-                  'gi'
-                ),
-                ' '
-              ))
-        }
-      }
-    }
+					modalWindow.classList
+						? modalWindow.classList.remove('open')
+						: (modalWindow.className = modalWindow.className.replace(
+								new RegExp(
+									'(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)',
+									'gi'
+								),
+								' '
+						  ))
+				}
+			}
+		}
 
-    /* Handling domready event IE9+ */
-    function ready(fn) {
-      if (document.readyState != 'loading') {
-        fn()
-      } else {
-        document.addEventListener('DOMContentLoaded', fn)
-      }
-    }
+		/* Handling domready event IE9+ */
+		function ready(fn) {
+			if (document.readyState != 'loading') {
+				fn()
+			} else {
+				document.addEventListener('DOMContentLoaded', fn)
+			}
+		}
 
-    /* Triggering modal window function after dom ready */
-    ready(openModal)
-    ready(closeModal)
-  })()
+		/* Triggering modal window function after dom ready */
+		ready(openModal)
+		ready(closeModal)
+	})()
 }
--- main.js
+++ main.js
@@ -7,71 +7,71 @@ const burgermenu = document.querySelector('.burgermenu')
 const closebtn = document.querySelector('.closebtn')
 const hiddenContent = document.querySelector('.hidden-content')
 const toggleContentButton = [
-  ...document.querySelectorAll('.toggle-content-button')
+	...document.querySelectorAll('.toggle-content-button')
 ]
 
 const loaded = localStorage.getItem('loaded')
 
 const load = () => {
-  if (window.location.pathname !== '/') {
-    document.querySelector('html').style.overflow = 'initial'
-    document.querySelector('body').style.overflow = 'initial'
-    hiddenContent.style.overflow = 'none'
-    return
-  }
-  if (loaded !== null) {
-    console.log('2')
-    allContent.classList.remove('remove-from-dom')
-    work.classList.remove('remove-from-dom')
-    body.style.overflow = 'initial'
-    document.querySelector('html').style.overflow = 'initial'
-    loadingScreen.remove()
-    return
-  }
+	if (window.location.pathname !== '/') {
+		document.querySelector('html').style.overflow = 'initial'
+		document.querySelector('body').style.overflow = 'initial'
+		hiddenContent.style.overflow = 'none'
+		return
+	}
+	if (loaded !== null) {
+		console.log('2')
+		allContent.classList.remove('remove-from-dom')
+		work.classList.remove('remove-from-dom')
+		body.style.overflow = 'initial'
+		document.querySelector('html').style.overflow = 'initial'
+		loadingScreen.remove()
+		return
+	}
 
-  setTimeout(() => {
-    loadingScreen.classList.add('hide-loading')
-    allContent.classList.remove('remove-from-dom')
-    work.classList.remove('remove-from-dom')
-    body.style.overflow = 'initial'
-    document.querySelector('html').style.overflow = 'initial'
-    localStorage.setItem('loaded', true)
-  }, 4200)
+	setTimeout(() => {
+		loadingScreen.classList.add('hide-loading')
+		allContent.classList.remove('remove-from-dom')
+		work.classList.remove('remove-from-dom')
+		body.style.overflow = 'initial'
+		document.querySelector('html').style.overflow = 'initial'
+		localStorage.setItem('loaded', true)
+	}, 4200)
 
-  setTimeout(() => {
-    loadingScreen.remove()
-  }, 4200)
+	setTimeout(() => {
+		loadingScreen.remove()
+	}, 4200)
 }
 
 burgermenu.addEventListener('click', () => {
-  openNav()
+	openNav()
 })
 closebtn.addEventListener('click', () => {
-  closeNav()
+	closeNav()
 })
 
 function openNav() {
-  document.querySelector('#mySidenav').style.zIndex = 2
-  document.getElementById('mySidenav').style.opacity = '1'
-  document.querySelector('nav').style.zIndex = 0
-  document.querySelector('body').style.overflow = 'hidden'
+	document.querySelector('#mySidenav').style.zIndex = 2
+	document.getElementById('mySidenav').style.opacity = '1'
+	document.querySelector('nav').style.zIndex = 0
+	document.querySelector('body').style.overflow = 'hidden'
 }
 
 function closeNav() {
-  document.getElementById('mySidenav').style.opacity = '0'
-  document.querySelector('.sidenav').style.zIndex = 0
-  document.querySelector('#mySidenav').style.zIndex = 0
-  document.querySelector('nav').style.zIndex = 2
-  document.querySelector('body').style.overflow = 'initial'
+	document.getElementById('mySidenav').style.opacity = '0'
+	document.querySelector('.sidenav').style.zIndex = 0
+	document.querySelector('#mySidenav').style.zIndex = 0
+	document.querySelector('nav').style.zIndex = 2
+	document.querySelector('body').style.overflow = 'initial'
 }
 load()
 
 toggleContentButton[0].addEventListener('click', () => {
-  toggleContentButton[0].classList.add('hide-button')
-  hiddenContent.classList.toggle('show-content')
+	toggleContentButton[0].classList.add('hide-button')
+	hiddenContent.classList.toggle('show-content')
 })
 
 toggleContentButton[1].addEventListener('click', () => {
-  toggleContentButton[0].classList.remove('hide-button')
-  hiddenContent.classList.toggle('show-content')
+	toggleContentButton[0].classList.remove('hide-button')
+	hiddenContent.classList.toggle('show-content')
 })
